f5568cdf255edca30ebce6fb61062784e6d90678
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-24 15:47:11 +0700

    membuat data pelajar

--- ParadigmaOOP_041.cpp/ParadigmaOOP_041.cpp.cpp
+++ ParadigmaOOP_041.cpp/ParadigmaOOP_041.cpp.cpp
@@ -37,3 +37,11 @@ public:
 	}
 };
 
+int main() {
+	pelajar siswa1("andi laksono", "SMAN 1 BANTUL");
+	cout << siswa1.perkenalan();
+	cout << "hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
+
