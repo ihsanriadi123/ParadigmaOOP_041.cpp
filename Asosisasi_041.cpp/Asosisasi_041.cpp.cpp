15bd9312a1dc729449b07d5bb5a10832ad0effd1
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-24 17:00:42 +0700

    awal pembuatan data kedua

--- Asosisasi_041.cpp/Asosisasi_041.cpp.cpp
+++ Asosisasi_041.cpp/Asosisasi_041.cpp.cpp
@@ -2,3 +2,16 @@
 #include <vector>
 using namespace std;
 
+class dokter;
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pnama) : nama(pnama) {
+		cout << "pasien \" " << nama << "\"tidak ada\n";
+	}
+	~pasien() {
+		cout << "pasien \" " << nama << "\"tidak ada\n";
+	}
+
+};
