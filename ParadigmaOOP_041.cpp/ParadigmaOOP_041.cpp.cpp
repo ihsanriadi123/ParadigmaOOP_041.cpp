099009ca152c0944651f8db09e51c232a388a48c
ihsan putra riadi <iputrariadi@gmail.com>
2023-05-24 15:26:31 +0700

    menampilkan string nama interger

--- ParadigmaOOP_041.cpp/ParadigmaOOP_041.cpp.cpp
+++ ParadigmaOOP_041.cpp/ParadigmaOOP_041.cpp.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pnama) :
+		nama(pnama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+
+};
 
